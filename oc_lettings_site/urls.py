0406a20e5f631b0711e80788b000fb0ecbdcf6b7
Franco-M <francomercau91@gmail.com>
2026-06-30 22:09:32 +0200

    pin production dependencies and remove sentry test endpoint

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,15 +5,7 @@ Includes URL patterns for the homepage, admin, and sub-applications.
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
 from . import views
-
-
-def trigger_error(request):
-    """Test view to trigger a Sentry error for verification."""
-    division_by_zero = 1 / 0
-    return division_by_zero
-
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -21,8 +13,3 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += [
-        path('sentry-debug/', trigger_error),
-    ]
